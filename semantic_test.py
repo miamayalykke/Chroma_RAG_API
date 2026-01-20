f5d04033579b13dca236a515dad934c0cd86eed8
miamayalykke <miamayalykke@gmail.com>
2026-01-20 13:34:32 +0100

    feat: adding second knowledge base and semantic tests

--- semantic_test.py
+++ semantic_test.py
@@ -14,6 +14,21 @@ def test_kubernetes_query():
 
     print("✅ Kubernetes query test passed")
 
+
+def test_docker_query():
+    response = requests.post("http://127.0.0.1:8000/query?q=What is Docker?")
+
+    if response.status_code != 200:
+        raise Exception(f"Server returned {response.status_code}: {response.text}")
+    
+    answer = response.json()["answer"]
+
+    #test for key concepts
+    assert "container" in answer.lower(), "Missing 'container' keyword"
+    assert "deploy" in answer.lower(), "Missing 'deploy' keyword"
+
+    print("✅ Kubernetes query test passed")
+
 if __name__ == "__main__":
     test_kubernetes_query()
     print("All semantic tests passed!")
